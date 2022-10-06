1e9dda8fef1493d699fe49f3c5c6ae2cb28b5cb8
Heba Shaheen <77917053+HebaShaheen@users.noreply.github.com>
2022-10-07 00:48:00 +0300

    Add files via upload

---
+++ connect/google-sheet.js
@@ -0,0 +1,20 @@
+
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbw0yp8fiRA53QBb9phVSLOs5QKroSOAZzlKqvHDQFerFgx2F-Z27vuwA1c-A3KJGSjD/exec'
+               const form = document.forms['google-sheet']
+             
+               form.addEventListener('submit', e => {
+                 e.preventDefault()
+                 fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+          .then( window.open("../files/thankyou.html"))
+           .catch(error => console.error('Error!', error.message))         
+               })
+               // const btn = document.querySelector(".button");
+   
+   // btn.classList.add("button--loading");
+   // btn.classList.remove("button--loading");
+   
+   
+
+ 
+  
